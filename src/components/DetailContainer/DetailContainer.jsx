138835fa6f6942d72484e84c418250eaa2cd8c6b
Martín Fiori <martinfiori@outlook.com>
2022-08-03 03:23:14 -0300

    detail done

--- src/components/DetailContainer/DetailContainer.jsx
+++ src/components/DetailContainer/DetailContainer.jsx
@@ -1,22 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getGameDetail } from "../../Redux/actions";
 import DetailItem from "../DetailItem/DetailItem";
+import Spinner from "../Spinner/Spinner.jsx";
 
 const DetailContainer = () => {
+	const [loading, setLoading] = useState(true);
 	const { id } = useParams();
 	const detailRedux = useSelector(state => state.gameDetail);
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(getGameDetail(id));
-	}, [dispatch, id]);
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+	}, [dispatch, id, loading]);
 
-	return (
-		<>
-			<DetailItem data={detailRedux} />
-		</>
-	);
+	return <>{loading ? <Spinner /> : <DetailItem data={detailRedux} />}</>;
 };
 
 export default DetailContainer;
--- src/components/DetailItem/DetailItem.jsx
+++ src/components/DetailItem/DetailItem.jsx
@@ -8,7 +8,6 @@ const DetailItem = ({
 		id,
 		minimum_system_requirements,
 		platform,
-		profile_url,
 		publisher,
 		release_date,
 		screenshots,
@@ -16,6 +15,7 @@ const DetailItem = ({
 		status,
 		thumbnail,
 		title,
+		game_url,
 	},
 }) => {
 	const [activeImage, setActiveImage] = useState(null);
@@ -30,12 +30,66 @@ const DetailItem = ({
 				<section className="detail__content">
 					<h3 className="detail__content--title">{title}</h3>
 					<p className="detail__content--description">{short_description}</p>
-					<span>{release_date}</span>
 				</section>
-				{/* <p>{JSON.stringify(minimum_system_requirements)}</p>
-				{screenshots?.map(el => (
-					<img key={el.id} src={el.image} className="screenshots" />
-				))} */}
+				<section className="detail__middle detail__list">
+					<h4>Game information:</h4>
+					<ul>
+						<li>
+							<p>developer: </p>
+							<span>{developer}</span>
+						</li>
+						<li>
+							<p>genre: </p>
+							<span>{genre}</span>
+						</li>
+						<li>
+							<p>publisher: </p>
+							<span>{publisher}</span>
+						</li>
+						<li>
+							<p>Release date: </p>
+							<span>{release_date}</span>
+						</li>
+						<li>
+							<p>status: </p>
+							<span>{status}</span>
+						</li>
+						<li>
+							<p>game's page: </p>
+							<span>{game_url}</span>
+						</li>
+					</ul>
+				</section>
+				<div className="detail__bottom">
+					<section className="detail__minimum detail__list">
+						<h4 className="detail__title">System Requirements</h4>
+						<ul>
+							{Object.entries(minimum_system_requirements).map((el, i) => (
+								<li key={i}>
+									<p>{el[0]}: </p>
+									<span>{el[1]}</span>
+								</li>
+							))}
+						</ul>
+					</section>
+					<section className="detail__gallery">
+						<img
+							src={activeImage ? activeImage : thumbnail}
+							alt="img"
+							className="gallery__main"
+						/>
+						<div className="gallery__secondary">
+							{screenshots?.map(el => (
+								<img
+									key={el.id}
+									src={el.image}
+									alt={el.image}
+									onClick={() => setActiveImage(el.image)}
+								/>
+							))}
+						</div>
+					</section>
+				</div>
 			</div>
 		</div>
 	);
